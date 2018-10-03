215879623232bc693c99200a150bb90955cae187
admin <admin@admins-MacBook-Pro.local>
2018-10-02 20:48:12 -0600

    hbs

--- server.js
+++ server.js
@@ -13,26 +13,34 @@ var app = express();
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.use(logger("dev"));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', express.static('./views'));
 app.set('views', path.join(__dirname, './views'));
-
 db._connect();
+
 app.get('/', function(req, res) {
-
-
-
+    res.render('home.hbs')
+});
+app.get('/saved', function(req, res) {
     Article.find({}, function(err, articles) {
         if (err) throw err;
         console.log(articles);
-        res.render('home.hbs', {articles:articles});
-      });
-
-});
-app.get('/saved', function(req, res) {
-    res.render('saved.hbs');
+        res.render('saved.hbs', {articles:articles});
+    });
 })
 
+app.post('/api', function(req, res) {
+    console.log(req.body);
+    // var data = new Article(req.body);
+    // data.save()
+    // .then(item => {
+    //     "Saved"
+    // })
+    // .catch(err => {
+    //     res.status(400).send("Unable to save");
+    // });
+});
 
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
